b6b49a591bc39becf7f296ac15d0fd95829d4054
Greg von Winckel <gregvw@gmail.com>
2025-08-31 08:30:32 -0600

    Added missing copyright banner to build_systems/meson/tests/test_adl_trait_shim.cpp
    Signed-off-by: Greg von Winckel <gregvw@gmail.com>

--- build_systems/meson/tests/test_adl_trait_shim.cpp
+++ build_systems/meson/tests/test_adl_trait_shim.cpp
@@ -1,4 +1,14 @@
 /**
+TInCuP - A library for generating and validating C++ customization point objects that use `tag_invoke`
+
+Copyright (c) National Technology & Engineering Solutions of Sandia, 
+LLC (NTESS). Under the terms of Contract DE-NA0003525 with NTESS, the U.S. 
+Government retains certain rights in this software.
+
+Questions? Contact Greg von Winckel (gvonwin@sandia.gov)
+*/
+
+/**
 TInCuP - ADL shim + cpo_impl integration test using std::vector (Meson subproject)
 */
 
