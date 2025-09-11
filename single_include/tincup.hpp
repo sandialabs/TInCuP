457e5f9746ec0a604f048858ed1f894fe828b184
Greg von Winckel <gregvw@gmail.com>
2025-09-10 20:24:59 -0600

    added missing const_cast_if_needed helper functions
    Signed-off-by: Greg von Winckel <gregvw@gmail.com>

--- single_include/tincup.hpp
+++ single_include/tincup.hpp
@@ -229,6 +229,21 @@ auto deref_if_needed(T&& x) noexcept(std::is_nothrow_move_constructible_v<T>)
 template<typename T>
 using deref_t = decltype(deref_if_needed(std::declval<T>()));
 
+template<typename T>
+auto const_cast_if_needed(T&& x) noexcept -> std::remove_const_t<std::remove_reference_t<T>>&
+requires std::is_lvalue_reference_v<T> && std::is_const_v<std::remove_reference_t<T>> {
+  return const_cast<std::remove_const_t<std::remove_reference_t<T>>&>(x);
+}
+
+template<typename T>
+auto const_cast_if_needed(T&& x) noexcept -> T&&
+requires (!std::is_lvalue_reference_v<T> || !std::is_const_v<std::remove_reference_t<T>>) {
+  return std::forward<T>(x);
+}
+
+template<typename T>
+using const_cast_t = decltype(const_cast_if_needed(std::declval<T>()));
+
 } // namespace tincup
 
 namespace tincup {
